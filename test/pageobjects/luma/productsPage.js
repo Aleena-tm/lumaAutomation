915e32c5ae27f33bd0c5bce5c24966ed89ffb0bc
Aleena Shaji <aleena.shaji@testingmavens.com>
2024-11-15 11:00:35 +0530

    Updated 71st it block

--- test/pageobjects/luma/productsPage.js
+++ test/pageobjects/luma/productsPage.js
@@ -426,6 +426,7 @@ class Productspage extends Common{
              await this.scrollAndClick(this.$updateQuantity(index));
          }
          await this.scrollAndClick(this.$proceedToCheckout());
+         await this.$spinner().waitForDisplayed({timeout:5000,reverse:true});
          await this.$shippingHeader.waitForDisplayed({timeout:5000, timeOutMsg:"Shipping page header should be displayed"});
      }
 
