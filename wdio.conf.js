bf60285f9daf34de0e18ec24b23ac6678b4a67a7
Aleena Shaji <aleena.shaji@testingmavens.com>
2024-11-18 10:55:35 +0530

    Updated yml file

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,9 +62,9 @@ export const config = {
     //
     capabilities: [{
         browserName: 'chrome',
-        // 'goog:chromeOptions': {
-        //     args: ['--headless','--disable-gpu', '--window-size=1920,1080'],
-        // },
+        'goog:chromeOptions': {
+            args: ['--headless','--disable-gpu', '--window-size=1920,1080'],
+        },
     }],
 
     //
