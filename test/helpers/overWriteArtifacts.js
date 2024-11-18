8e42420515f82e1d39e4cf4a6d1d6589e33440db
Aswin Pathiramanna <aswin.pathiramanna@testingmavens.com>
2024-11-18 12:06:17 +0530

    Sign up code changes

--- test/helpers/overWriteArtifacts.js
+++ test/helpers/overWriteArtifacts.js
@@ -1,19 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-
+ 
 const helpers = {
     writeToTempFile: async (filename, content) => {
         // Define the directory where the file will be saved
         const dir = path.join(process.cwd(), 'test', '.artifacts'); // Use process.cwd() for the current working directory
-        
+       
         // Ensure the directory exists
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
         }
-
+ 
         // Define the full path for the file
         const filePath = path.join(dir, `${filename}.txt`); // You can change the file extension if needed
-
+ 
         // Write the content to the file
         try {
             await fs.promises.writeFile(filePath, content);
@@ -23,5 +23,5 @@ const helpers = {
         }
     }
 };
-
+ 
 export default helpers;
